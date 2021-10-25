857368fc7fb4a4418f9b3aa728d4c380a2ce39e9
juancruz_94_ <juancruz_94_@hotmail.com>
2021-10-25 20:57:13 -0300

    feat: sendEmail helper

---
+++ tp/src/shared/helpers/sendEmail.js
@@ -0,0 +1,22 @@
+// Send a mail with email, subject and html given
+export function sendEmail(email, subject, html) {
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: 'juancruzalmazan1994@gmail.com', // Verified sendgrid email, change it to grupo11unlp@gmail.com in the future
+    subject: subject,
+    html: html,
+  };
+  (async () => {
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    }
+  })();
+}
