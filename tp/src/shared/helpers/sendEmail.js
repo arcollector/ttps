081ee6ed53d617ed3c8184ff0f36ebac02a5760e
juancruz_94_ <juancruz_94_@hotmail.com>
2021-10-25 21:31:19 -0300

    chore: sendgrid dependency

--- tp/src/shared/helpers/sendEmail.js
+++ tp/src/shared/helpers/sendEmail.js
@@ -1,7 +1,7 @@
 // Send a mail with email, subject and html given
 export function sendEmail(email, subject, html) {
   const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Update the development environment with your SENDGRID_API_KEY
   const msg = {
     to: email,
     from: 'juancruzalmazan1994@gmail.com', // Verified sendgrid email, change it to grupo11unlp@gmail.com in the future
