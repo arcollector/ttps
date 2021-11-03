317049ae04cc7b590aebe9a5007e842b4e8a32ad
Martin Ruiz <arcollector@gmail.com>
2021-11-03 17:06:50 -0300

    feat: medical exams update by fernando

--- tp/src/MedicalExams/containers/MedicalExams.js
+++ tp/src/MedicalExams/containers/MedicalExams.js
@@ -31,6 +31,7 @@ export function MedicalExams(props) {
     const [states, setStates] = useState([]);
     const [filterStates, setFilterStates] = useState(null);
     const [reloading, setReloading] = useState(false);
+    const [viewFilter, setViewFilter] = useState({estado:"todos"})
 
 
 
@@ -154,7 +155,12 @@ export function MedicalExams(props) {
     }, [])
 
     
-
+    const viewOnChange=(e)=>{
+        
+        setViewFilter({ estado:e.target.value});
+        console.log(viewFilter);
+        
+    }
 
 
  
@@ -163,19 +169,36 @@ export function MedicalExams(props) {
 
 
         <div className="estudios-content">
+
+                <select name="estado" onChange={viewOnChange} multiple="" class="ui fluid dropdown">
+                <option value="todos">Todos</option>
+                <option value="enviarPresupuesto">Estudios sin presupuesto enviado</option>
+                <option value="esperandoComprobante">Estudios sin recibir comprobante de pago</option>
+                <option value="enviarConsentimiento">Estudios sin enviar consentimiento</option>
+                <option value="esperandoConsentimiento">Estudios sin recibir consentimiento informado</option>
+                <option value="esperandoTurno">Estudios sin turno</option>
+                <option value="esperandoTomaDeMuestra">Estudios sin toma de muestra</option>
+                <option value="esperandoRetiroDeMuestra">Estudios con muestra sin retirar</option>
+                <option value="esperandoLote">Estudios esperando lote</option>
+                
+                
+            </select>
+
+
+
             {filterStates&& filterStates?.map((exams, data)=>{
                 
                 return( 
                     <>
                     
-                      {exams==="enviarPresupuesto" && filterStates[exams].length>0 && <h3>Estudios que requieren enviar presupuesto</h3>}
-                      {exams==="enviarConsentimiento" &&  filterStates[exams].length>0 && <h3>Estudios que requieren enviar consentimiento para su firma</h3>}
-                      {exams==="esperandoComprobante" &&  filterStates[exams].length>0 &&  <h3>Estudios impagos</h3>}
-                      {exams==="esperandoConsentimiento" &&  filterStates[exams].length>0 &&  <h3>Estudios que esperan recibir consentimiento firmado </h3>}
-                      {exams==="esperandoTurno" &&  filterStates[exams].length>0 && <h3>Estudios sin turno </h3>}
-                      {exams==="esperandoTomaDeMuestra" &&  filterStates[exams].length>0 && <h3>Estudios a la espera de la toma de muestra </h3>}
-                      {exams==="esperandoRetiroDeMuestra" &&  filterStates[exams].length>0 &&  <h3>Estudios a la espera del retiro de muestra </h3>}
-                      {exams==="esperandoLote" &&  filterStates[exams].length>0 &&  <h3>Estudios a la espera de lote de muestras </h3>}
+                      {exams==="enviarPresupuesto" && filterStates[exams].length>0 && (viewFilter.estado==="enviarPresupuesto"||viewFilter.estado==="todos") && <h3>Estudios que requieren enviar presupuesto</h3>}
+                      {exams==="enviarConsentimiento" &&  filterStates[exams].length>0 && (viewFilter.estado==="enviarConsentimiento"||viewFilter.estado==="todos") &&<h3>Estudios que requieren enviar consentimiento para su firma</h3>}
+                      {exams==="esperandoComprobante" &&  filterStates[exams].length>0 && (viewFilter.estado==="esperandoComprobante"||viewFilter.estado==="todos") && <h3>Estudios impagos</h3>}
+                      {exams==="esperandoConsentimiento" &&  filterStates[exams].length>0 && (viewFilter.estado==="esperandoConsentimiento"||viewFilter.estado==="todos") && <h3>Estudios que esperan recibir consentimiento firmado </h3>}
+                      {exams==="esperandoTurno" &&  filterStates[exams].length>0 && (viewFilter.estado==="esperandoTurno"||viewFilter.estado==="todos") &&<h3>Estudios sin turno </h3>}
+                      {exams==="esperandoTomaDeMuestra" &&  filterStates[exams].length>0 && (viewFilter.estado==="esperandoTomaDeMuestra"||viewFilter.estado==="todos") &&<h3>Estudios a la espera de la toma de muestra </h3>}
+                      {exams==="esperandoRetiroDeMuestra" &&  filterStates[exams].length>0 && (viewFilter.estado==="esperandoRetiroDeMuestra"||viewFilter.estado==="todos") && <h3>Estudios a la espera del retiro de muestra </h3>}
+                      {exams==="esperandoLote" &&  filterStates[exams].length>0 && (viewFilter.estado==="esperandoLote"||viewFilter.estado==="todos") && <h3>Estudios a la espera de lote de muestras </h3>}
 
                     <div className="section-state">
 
@@ -185,42 +208,43 @@ export function MedicalExams(props) {
                             {filterStates[exams].map(exam=>{
                             
                                         return( 
-                                            <div className="contenedor-tarjeta">
-                                        <div className="ui card">
-                                                
-                                                    {patients &&<div className="header">{patients[exam.idPatient].nombreCompleto}</div>}
-                                                
-                                                
-                                                <div className="content">
-                                                    <h4 className="ui sub header">Estudios</h4>
-                                                    <ol className="ui list">
-                                                    {exam.arraySelected==="true"&&( <li value="*" key="array">Array</li>)}
-                                                    {exam.genomaSelected==="true"&&( <li value="*"key="genoma">Genoma</li>)}
-                                                    {exam.cariotipoSelected==="true"&&( <li value="*" key="cariotipo">Cariotipo</li>)}
-                                                    {exam.exomaSelected==="true"&&( <li value="*" key="exoma">Exoma</li>)}
-                                                    {exam.carrierSelected==="true"&&( <li value="*" key="carrier">Carrier</li>)}
+                                            <>
+                                            {(viewFilter.estado===exams||viewFilter.estado==="todos") && <div className="contenedor-tarjeta">
+                                                <div className="ui card">
                                                         
-                                                    </ol>
-                                                    <h4 className="ui sub header">Medico Derivante:</h4>
-                                                    <ol className="ui list">
-                                                        {doctors && <li key="medic" value="*">{doctors[exam.idMedic]}</li>}
-                                                    </ol>
-                                                    
-                                                    
+                                                            {patients &&<div className="header">{patients[exam.idPatient].nombreCompleto}</div>}
+                                                        
+                                                        
+                                                        <div className="content">
+                                                            <h4 className="ui sub header">Estudios</h4>
+                                                            <ol className="ui list">
+                                                            {exam.arraySelected==="true"&&( <li value="*" key="array">Array</li>)}
+                                                            {exam.genomaSelected==="true"&&( <li value="*"key="genoma">Genoma</li>)}
+                                                            {exam.cariotipoSelected==="true"&&( <li value="*" key="cariotipo">Cariotipo</li>)}
+                                                            {exam.exomaSelected==="true"&&( <li value="*" key="exoma">Exoma</li>)}
+                                                            {exam.carrierSelected==="true"&&( <li value="*" key="carrier">Carrier</li>)}
+                                                                
+                                                            </ol>
+                                                            <h4 className="ui sub header">Medico Derivante:</h4>
+                                                            <ol className="ui list">
+                                                                {doctors && <li key="medic" value="*">{doctors[exam.idMedic]}</li>}
+                                                            </ol>
+                                                            
+                                                            
+                                                        </div>
+            
+                                                        {exams==="enviarPresupuesto" && <EnviarPresupuesto user={user} exam={exam} setReloading={setReloading}/>}
+                                                        {exams==="enviarConsentimiento" && <EnviarConsentimiento  user={user} exam={exam} setReloading={setReloading} />}
+                                                        {exams==="esperandoComprobante" && <SubirComprobante user={user} exam={exam} setReloading={setReloading}/>}
+                                                        {exams==="esperandoConsentimiento" && <SubirConsentimiento user={user} exam={exam} setReloading={setReloading}/>}
+                                                        {exams==="esperandoTurno" && <ReservarTurno user={user} exam={exam} setReloading={setReloading}/>}
+                                                        {exams==="esperandoTomaDeMuestra" && <TomarMuestra user={user} exam={exam} setReloading={setReloading}/>}
+                                                        {exams==="esperandoRetiroDeMuestra" && <RetirarMuestra user={user} exam={exam} setReloading={setReloading}/>}
+                                                        
                                                 </div>
-     
-                                                {exams==="enviarPresupuesto" && <EnviarPresupuesto user={user} exam={exam} setReloading={setReloading}/>}
-                                                {exams==="enviarConsentimiento" && <EnviarConsentimiento  user={user} exam={exam} setReloading={setReloading} />}
-                                                {exams==="esperandoComprobante" && <SubirComprobante user={user} exam={exam} setReloading={setReloading}/>}
-                                                {exams==="esperandoConsentimiento" && <SubirConsentimiento user={user} exam={exam} setReloading={setReloading}/>}
-                                                {exams==="esperandoTurno" && <ReservarTurno user={user} exam={exam} setReloading={setReloading}/>}
-                                                {exams==="esperandoTomaDeMuestra" && <TomarMuestra user={user} exam={exam} setReloading={setReloading}/>}
-                                                {exams==="esperandoRetiroDeMuestra" && <RetirarMuestra user={user} exam={exam} setReloading={setReloading}/>}
                                                 
-                                        </div>
-                                        
-                                        </div>
-                                        
+                                        </div>}
+                                        </>
                                         );
 
                         })}
