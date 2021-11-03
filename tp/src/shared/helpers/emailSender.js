d3cf5a684ec063754d394402ec881e722574d5cc
Martin Ruiz <arcollector@gmail.com>
2021-11-02 23:30:57 -0300

    fix: security breach

--- tp/src/shared/helpers/emailSender.js
+++ tp/src/shared/helpers/emailSender.js
@@ -1,7 +1,7 @@
 // Send a mail with email, subject and html given
 export function sendEmail(email, subject, html) {
   const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey('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'); // Update the development environment with your SENDGRID_API_KEY
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Update the development environment with your SENDGRID_API_KEY
   const msg = {
     to: email,
     from: 'grupo11unlp@gmail.com', // Verified sendgrid email
