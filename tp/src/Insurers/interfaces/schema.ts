2f9f05a9e3207e40c66ec8e9f2310f98ff48aabd
Martin Ruiz <arcollector@gmail.com>
2021-11-09 22:18:28 -0300

    wip: added tests for insurers

--- tp/src/Insurers/interfaces/schema.ts
+++ tp/src/Insurers/interfaces/schema.ts
@@ -3,7 +3,8 @@ import * as yup from 'yup';
 export const schema = yup.object().shape({
   nombre: yup
     .string()
-    .required('El nombre de la obra social no puede estar vacio')
+    .required()
+    .min(1, 'El nombre de la obra social no puede estar vacio')
 });
 
 export const validators = {
